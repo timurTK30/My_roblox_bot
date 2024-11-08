991fcb80e33ae1f41a67e780e6293e114dad4f51
timurTK30 <tymiramongas@gmail.com>
2024-11-08 21:02:40 +0100

    ultra kibir bot 13300

--- src/main/java/com/example/demo/handlers/UserCallbackHanlers.java
+++ src/main/java/com/example/demo/handlers/UserCallbackHanlers.java
@@ -8,7 +8,7 @@ public class UserCallbackHanlers implements BasicHandlers{
         return callbackData.matches(
                 "(Зарегистрировать в системе|😀|😡|ALL|HORROR|ADVENTURE" +
                         "|SHOOTER|TYCOON|SURVIVAL|Оставить заяву.*)"
-        )
+        );
     }
 
     @Override
