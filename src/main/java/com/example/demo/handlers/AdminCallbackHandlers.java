6cb431018c818a091b6429c0c459f41acbe48880
timurTK30 <tymiramongas@gmail.com>
2024-11-22 19:38:37 +0100

    ultra kibir bot 14000 [Domaska update]

--- src/main/java/com/example/demo/handlers/AdminCallbackHandlers.java
+++ src/main/java/com/example/demo/handlers/AdminCallbackHandlers.java
@@ -9,14 +9,49 @@ public class AdminCallbackHandlers implements BasicHandlers{
     public boolean canHandle(String callbackData) {
         String replacedData = callbackData.replaceAll("[^a-zA-Zа-яА-ЯёЁ\\s]", "").trim();
         return replacedData.matches(
-                "(^Прочитать сообщение от юзера|Перезагрузить бота| *.Статистика использования бота" +
-                        "| *.Отправить сообщение|Доступные квесты|Квест меню|Создать квест|Удалить старие квесты" +
-                        "|Прочитать доступные игры|User.*|)"
+                "(^Прочитать сообщение от юзера|Перезагрузить бота|Статистика использования бота" +
+                        "|Доступные квесты|Квест меню|Создать квест|Удалить старие квесты" +
+                        "|Прочитать доступные игры|User.*|Отправить сообщение|Редоктировать.*" +
+                        "Добавить награду для квеста.*|Добавить описание для квеста.*" +
+                        "|Добавить игру для квеста.*| *.Изменить на.*)"
         );
     }
 
     @Override
-    public void handle(Long chatId, String text) {
+    public void handle(Long chatId, String callbackData) {
+        switch (callbackData) {
+            case "Прочитать сообщение от юзера":
+                break;
+            case "Перезагрузить бота":
+                break;
+            case "Статистика использования бота":
+                break;
+            case "Доступные квесты":
+                break;
+            case "Квест меню":
+                break;
+            case "Создать квест":
+                break;
+            case "Удалить старие квесты":
+                break;
+            case "Прочитать доступные игры":
+                break;
+            case "Отправить сообщение":
+                break;
+            default:
+                if(callbackData.startsWith("User")){
 
+                } else if (callbackData.startsWith("Редоктировать")) {
+
+                } else if (callbackData.startsWith("Добавить награду для квеста")) {
+
+                } else if (callbackData.startsWith("Добавить описание для квеста")) {
+
+                } else if (callbackData.startsWith("Добавить игру для квеста")) {
+
+                } else if (callbackData.contains("Изменить на")) {
+
+                }
+        }
     }
 }
